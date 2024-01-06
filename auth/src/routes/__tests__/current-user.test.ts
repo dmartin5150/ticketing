faf00e72e4dcaef11d2b113776fc8ed07fb92e8b
David Martin <dmartin5150@icloud.com>
2024-01-06 10:24:21 -0600

    last minute change

--- auth/src/routes/__tests__/current-user.test.ts
+++ auth/src/routes/__tests__/current-user.test.ts
@@ -11,7 +11,7 @@ it ('responds with details about the current', async () => {
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
-    .expect(200);
+    .expect(400);
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
 
